5f663105ddf280d97d4b40ac4e5db36160b42c16
sriramvalluri <valluri.sriram7@gmail.com>
2020-05-19 13:01:04 +0530

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://username:<password>@blog-dev-shard-00-00-yncbc.mongodb.net:27017,blog-dev-shard-00-01-yncbc.mongodb.net:27017,blog-dev-shard-00-02-yncbc.mongodb.net:27017/test?ssl=true&replicaSet=blog-dev-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
