7e9805df7f2f1557e2cefdb5cb273842afc89290
sriramvalluri <valluri.sriram7@gmail.com>
2020-05-24 16:17:47 +0530

    added implemention of Query Caching functionality with Redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -13,13 +14,15 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
 
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
